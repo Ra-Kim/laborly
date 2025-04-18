f0c52c66367c796c29221e51376a737839e7b597
CHISOM TONY-ALAEDE <79312277+Ceecee-T@users.noreply.github.com>
2025-04-18 08:47:42 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
 import Home from './Pages/Home'
 import Artisans from './Pages/Artisans'
 import ArtisanProfile from './Pages/ArtisanProfile'
@@ -7,6 +6,7 @@ import Blog from './Pages/Blog'
 import NotFound from './Pages/NotFound'
 import Login from './Pages/auth/login'
 import Signup from './Pages/auth/signup'
+import Navbar from './components/Navbar'
 
 
 
