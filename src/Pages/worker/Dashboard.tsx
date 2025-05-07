a9fbf6fd0ed81fd57b5d1f44f2a640fd84fc333b
CHISOM TONY-ALAEDE <79312277+Ceecee-T@users.noreply.github.com>
2025-05-07 18:02:46 +0100

    hold

--- src/Pages/worker/Dashboard.tsx
+++ src/Pages/worker/Dashboard.tsx
@@ -1,9 +1,9 @@
 const WorkerDashbaord = () => {
-	return (
-		<div>
-			<h3>Dashboard</h3>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center  justify-center h-[80vh]">
+      <p className="text-3xl lg:text-6xl font-bold">Welcome to Laborly</p>
+    </div>
+  );
 };
 
 export default WorkerDashbaord;
